aa54a6a18bc9773ea193d102531ae97fed00df6c
kapil rawat <krkapil45@gmail.com>
2025-10-27 11:51:11 +0530

    added gitignore

--- config/db.php
+++ config/db.php
@@ -1,9 +1,6 @@
 <?php
 require "env.php";
 
-// var_dump(DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-// die();
 $conn= new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
